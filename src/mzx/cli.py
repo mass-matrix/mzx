f955dc25a457732abc6349c471e1899d99d598e0
Mike Freitas <mike.freitas@gmail.com>
2025-05-12 11:09:25 -0400

    added simple params to msconver and mzx cli

--- src/mzx/cli.py
+++ src/mzx/cli.py
@@ -9,11 +9,78 @@ def main():
     )
     parser.add_argument("file", type=str, help="The file to convert.")
     parser.add_argument("--type", type=str, default="mzml", help="The output format.")
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        default=False,
+        help="Overwrite the output file if it exists.",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        default=False,
+        help="Enable debug mode.",
+    )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Enable verbose output.",
+    )
+    parser.add_argument(
+        "--index",
+        action="store_true",
+        default=False,
+        help="Index the output file.",
+    )
+    parser.add_argument(
+        "--sortbyscan",
+        action="store_true",
+        default=False,
+        help="Sort the output file by scan.",
+    )
+    parser.add_argument(
+        "--peak_picking",
+        action="store",
+        choices=["off", "msms", "ms1"],
+        default="msms",
+        help="Enable peak picking.",
+    )
+    parser.add_argument(
+        "--remove_zeros",
+        action="store_true",
+        default=True,
+        help="Remove zeros from the output file.",
+    )
+    parser.add_argument(
+        "--vendor",
+        type=str,
+        default=None,
+        help="Specify the vendor of the raw file.",
+    )
+    parser.add_argument("--output", type=str, default=None, help="The output file.")
     args = parser.parse_args()
-    infile = args.file
 
-    vendor = get_vendor(infile)
-    _outfile = convert_raw_file(infile, vendor)
+    params = {
+        "infile": args.file,
+        "index": args.index,
+        "sortbyscan": args.sortbyscan,
+        "peak_picking": args.peak_picking,
+        "remove_zeros": args.remove_zeros,
+        "vendor": None,
+        "outfile": None,
+        "type": args.type,
+        "overwrite": args.overwrite,
+        "debug": args.debug,
+        "verbose": args.verbose,
+    }
+
+    if args.vendor is None:
+        params["vendor"] = get_vendor(params)
+    else:
+        params["vendor"] = args.vendor
+
+    status = convert_raw_file(params)
 
 
 if __name__ == "__main__":
